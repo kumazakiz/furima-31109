45f36a2c2ebb3ca5fef6b1635dfb90340c535e4f
kumazakiz <tenka2dsk@gmail.com>
2020-11-10 20:12:22 +0900

    itemモデルの単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    title            {"test"}
+    explanation      {"test"}
+    category_id      {2}
+    item_status_id   {2}
+    shipping_cost_id {2}
+    shipping_area_id {2}
+    days_to_ship_id  {2}
+    selling_price    {10000}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
