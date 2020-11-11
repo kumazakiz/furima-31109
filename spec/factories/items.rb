4c48aabc398ad79b9e5f4e4f39c6fbe5f2ab67e5
kumazakiz <tenka2dsk@gmail.com>
2020-11-11 14:50:44 +0900

    コードレビュー修正依頼(2)
    修正内容
    ①selling_price.jsのconsole.log~の記述を全部削除する。
    ②items.rbのActiveHashにかかってたFactoryBotの記述を削除する。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,7 @@ FactoryBot.define do
   factory :item do
     title            { 'test' }
     explanation      { 'test' }
-    category_id      { 2 }
-    item_status_id   { 2 }
-    shipping_cost_id { 2 }
-    shipping_area_id { 2 }
-    days_to_ship_id  { 2 }
-    selling_price    { 10_000 }
+    selling_price    { 10000 }
     association :user
 
     after(:build) do |item|
