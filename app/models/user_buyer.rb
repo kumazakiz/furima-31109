d4b27b9dab2bf5a4cae8ce006f5a38fe3c579762
kumazakiz <tenka2dsk@gmail.com>
2020-11-14 18:23:49 +0900

    購入者(buyer), 購入者情報(shipping_address)モデルの作成
    ①アソシエーションの設定
    ②テーブルの作成

--- app/models/user_buyer.rb
+++ app/models/user_buyer.rb
@@ -7,8 +7,9 @@ class UserBuyer
     validates :prefectures_id
     validates :municipality
     validates :address
-    validates :phone_number, 
+    validates :phone_number
   end
+
     validates :postal_code, format: {with: /\A\d{11}\z/, message: "write within 11 characters"}
     validates :phone_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
 
@@ -16,4 +17,5 @@ class UserBuyer
     Buyer.create(user_id: user_id, item_id: item_id)
     ShippingAddress.create(postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number)
   end
+
 end
