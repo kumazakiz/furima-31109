9d08901c69b8c3449d4c37cf8009958f048e2c5c
kumazakiz <tenka2dsk@gmail.com>
2020-11-14 12:54:55 +0900

    Formオブジェクト作成(2)
    ①バリデーションの設定
    ②保存先モデルの設定

--- app/models/user_buyer.rb
+++ app/models/user_buyer.rb
@@ -1,4 +1,19 @@
 class UserBuyer
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefectures_id, :municipality, :address, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :postal_code
+    validates :prefectures_id
+    validates :municipality
+    validates :address
+    validates :phone_number, 
+  end
+    validates :postal_code, format: {with: /\A\d{11}\z/, message: "write within 11 characters"}
+    validates :phone_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+
+  def save
+    Buyer.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(postal_code: postal_code, prefectures_id: prefectures_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number)
+  end
 end
